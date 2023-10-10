09304e7d83cab550bac14521c68b34bc5045658c
jpsamarino <jpsamarino3@hotmail.com>
2023-10-10 12:13:51 -0300

    finish exercises for this week

--- exercises/src/bin/ex_018_numbers.rs
+++ exercises/src/bin/ex_018_numbers.rs
@@ -1,6 +1,6 @@
 fn main() {
     let mut sum = 0;
-    for i in -3..2 {
+    for i in -3..3 { // -3 -2 -1 0 1 2 (using ..= to include the last value) or -3..3 (using .. to exclude the last value)
         sum += i
     }
 
--- exercises/src/bin/ex_019_numbers.rs
+++ exercises/src/bin/ex_019_numbers.rs
@@ -1,8 +1,10 @@
 // Fill the blanks
 use std::ops::{Range, RangeInclusive};
 fn main() {
-    assert_eq!((1..__), Range{ start: 1, end: 5 });
-    assert_eq!((1..__), RangeInclusive::new(1, 5));
+    assert_eq!((1..5), Range{ start: 1, end: 5 }); // it return a Range
+    assert_eq!((1..=5), RangeInclusive::new(1, 5)); //it return a RangeInclusive
+    assert_eq!(RangeInclusive::new(1, 5).contains(&5), true); // it return a bool
+    assert_eq!(RangeInclusive::new(1, 5).collect::<Vec<i32>>(),[1,2,3,4,5]); // it return a bool
 
     println!("Success!");
 }
