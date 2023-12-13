46714807ac6e2c3d83195f403b970513a8cbe19e
jpsamarino <jpsamarino3@hotmail.com>
2023-12-13 14:30:13 -0300

    feat: add sketch of classes in path ideas

---
+++ basic/src/bin/006e00-operators-basic.rs
@@ -0,0 +1,11 @@
+fn main(){
+    //exercise 1
+    // complete the code below (replace __ to correct answer)
+    let number = 3_i32;
+    let operation:__ = number + 2 == 5;
+    
+    
+
+    
+
+}
