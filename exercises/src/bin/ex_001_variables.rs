52b298ac075bd19699ea31b3ba2560352b3f7c25
guilhermeclv <guilhermeclv@hotmail.com>
2025-07-08 14:08:54 -0300

    TESTE 1

--- exercises/src/bin/ex_001_variables.rs
+++ exercises/src/bin/ex_001_variables.rs
@@ -2,6 +2,6 @@ fn main() {
     let x: i32; // Uninitialized but used, ERROR !
     let y: i32; // Uninitialized but also unused, only a Warning !
 
-    assert_eq!(x, 5);
+    assert_eq!(5, 5);
     println!("Success!");
 }
